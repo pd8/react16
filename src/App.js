3f21429b8ef2fadc7b8b4c0a69d93386ac8c768c
Pip <pipjdunne@gmail.com>
2018-03-27 21:50:40 +0100

    extract welcome message to subclass inside app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const user = "Pip";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Welcome />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    return (
+      <h1 className="App-title">Welcome to React, {user}</h1>
     );
   }
 }
